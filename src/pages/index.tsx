4790e9a65f50389a3b2fdb1bbf1006ffe3af8c55
johnathew <jakornegay01@gmail.com>
2023-04-03 15:54:45 -0500

    dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,18 @@ import Link from "next/link";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { FiGithub } from "react-icons/fi";
 import { CiLinkedin, CiLocationOn } from "react-icons/ci";
-import { SlSocialSpotify } from "react-icons/sl";
 import { IoIosSunny } from "react-icons/io";
-import { MdOutlineDarkMode } from "react-icons/md";
+
 import { HiOutlineMail } from "react-icons/hi";
+import { BsSun } from "react-icons/bs";
 import Image from "next/image";
-import gradPic from "public/gradPic.jpeg"
+import gradPic from "public/gradPic.jpeg";
 import { api } from "~/utils/api";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  // never want user to connect directly to a DB
   const { data } = api.comments.getAll.useQuery();
+  const [darkMode, setDarkMode] = useState(true);
   const user = useUser();
 
   return (
@@ -23,87 +24,119 @@ const Home: NextPage = () => {
       <Head>
         <title>Resume</title>
       </Head>
-      <header className="flex w-full justify-center">
+      <header
+        className={`flex w-full justify-center ${darkMode ? "dark" : ""}`}>
         <div className="m-0 flex w-full items-center justify-between border-x-4 border-b-2 border-x-slate-200 border-b-slate-200 bg-slate-200 p-2 text-slate-800 md:w-3/4 md:text-lg">
           <Link href="/">
             <div className="flex justify-start space-x-2">
-              <nav className="md:align-center rounded-lg bg-gray-500 p-2">
+              <nav className="md:align-center rounded-lg border-2 bg-slate-400 p-2">
                 Resume
               </nav>
               <nav className="rounded-lg p-2">Blog App</nav>
             </div>
           </Link>
-          <div className="ml-2 flex h-10 w-auto space-x-2 p-2 text-2xl">
-            <AiOutlineInstagram />
-            <FiGithub />
-            <CiLinkedin />
-            <SlSocialSpotify />
-            <MdOutlineDarkMode />
-            <IoIosSunny />
+          <div className="ml-2 flex h-10 w-auto space-x-2 rounded-lg p-3 text-2xl">
+            <AiOutlineInstagram className="rounded-lg hover:bg-amber-400" />
+            <FiGithub className="rounded-lg hover:bg-amber-400" />
+            <CiLinkedin className="rounded-lg hover:bg-amber-400" />
+            {darkMode && (
+              <IoIosSunny
+                onClick={() => setDarkMode((prev) => !prev)}
+                className="hover:bg-amber-400"
+              />
+            )}
+            {!darkMode && (
+              <BsSun
+                onClick={() => setDarkMode((prev) => !prev)}
+                className="hover:bg-amber-400"
+              />
+            )}
           </div>
         </div>
       </header>
-      <main className="flex flex-col items-center h-screen  text-zinc-50 w-full">
-      <div className="border-b-2 border-b-slate-200 border-x-4 border-x-slate-200 py-6 px-5 flex flex-col w-full md:w-3/4">
-          <div className=" w-full items-center justify-start border-white pb-2 px-1 flex">
+      <main className={`flex h-screen w-full flex-col text-black items-center ${darkMode ? "dark" : ""} `}>
+        <div className="flex dark:text-slate-200 dark:bg-black bg-slate-300  w-full flex-col border-x-4 border-b-2 border-x-slate-200 border-b-slate-200 px-5 py-6 md:w-3/4">
+          <div className=" flex w-full items-center justify-start border-white px-1 pb-2">
             <Image
               src={gradPic}
               alt="bio degree lol"
-              priority={true}
-              className="h-12 rounded-full w-auto"
+              className="h-12 w-auto rounded-full"
             />
-            <div className="pl-3 flex flex-col items-left">
-              <h1 className="font-medium text-2xl hover:text-yellow-400 justify-center">
+            <div className="items-left flex flex-col pl-3">
+              <h1 className="justify-center text-2xl font-medium hover:text-yellow-400">
                 John A. Kornegay
               </h1>
               <h2 className="text-sm font-light">Frontend Developer</h2>
-              </div>
-              
+            </div>
           </div>
-          <div className="border-b-2 py-2 w-full justify-end">
-              <p>
+          <div className="w-full justify-end border-b-2 py-2">
+            <p>
               A self-taught and highly motivated individual seeking to gain
               experience as a front-end developer through an internship
               opportunity.
             </p>
-            </div>
-          <div className="flex p-1 w-3/4 items-end pt-2">
+          </div>
+          <div className="flex w-3/4 items-end p-1 pt-2">
             <CiLocationOn className="text-2xl text-yellow-200" />
-            <h2 className="text-md font-normal pl-0.5 tracking-wide">
-              {" "}
+            <h2 className="text-md pl-0.5 font-normal tracking-wide">
               McAllen, TX
             </h2>
           </div>
-          <div className="flex pl-2 w-3/4 items-center">
+          <div className="flex w-3/4 items-center pl-2">
             <HiOutlineMail className="text-lg text-yellow-200" />
-            <h2 className="text-md font-light pl-1 tracking-wide">
+            <h2 className="text-md pl-1 font-light tracking-wide">
               Jakornegay01@gmail.com
             </h2>
           </div>
         </div>
-        <section className="border-b-2 border-b-slate-200 border-x-4 border-x-slate-50 flex flex-col justify-between  w-full px-5 py-6 md:w-3/4">
-          <div className="justify-center py-2">
-            <h2 className="font-bold text-2xl underline">Experience</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, suscipit.</p>
-            <h2 className="font-bold text-2xl underline">Languages</h2>
+        <section className="flex w-full flex-col bg-slate-300 dark:bg-black justify-between border-x-4 border-b-2 border-x-slate-50  border-b-slate-200 px-5 py-6 md:w-3/4">
+          <div className="justify-center py-2 bg-slate-300 text-black dark:text-slate-200 dark:bg-black ">
+            <h2 className="text-2xl font-bold underline">EDUCATION</h2>
+            <div className="w-1/2 justify-between rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+              <p className="font-semibold">Bachelor of Science in Biology</p>
+              <p>Graduation: 2016</p>
+            </div>
+            <p className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+              Texes A&M Univeristy - College Station, TX
+            </p>
+            <h2 className="text-2xl font-bold underline">Languages</h2>
             <ul>
-              <li>Javascript</li>
-              <li>React JS</li>
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                Javascript
+              </li>
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                React JS
+              </li>
             </ul>
-            <h2 className="font-bold text-2xl underline">Projects</h2>
+            <h2 className="text-2xl font-bold underline">Projects</h2>
             <ul>
-              <li>Calculator</li>
-              <li>Memory Game</li>
-              <li>Blog App</li>
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                Calculator
+              </li>
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                Memory Game
+              </li>
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                Blog App
+              </li>
             </ul>
-            <h2 className="font-bold text-2xl underline">Skills</h2>
-            <ul>
-              <li>Adobe Photoshop</li>
+            <h2 className="text-2xl font-bold underline">Skills</h2>
+            <ul className="">
+              <li className="w-1/2 rounded-lg p-2 hover:bg-slate-600 hover:text-yellow-400 hover:underline">
+                Adobe Photoshop
+              </li>
             </ul>
           </div>
         </section>
-        {!user.isSignedIn && <SignInButton />}
+        {!user.isSignedIn && (
+          <div className="m-2 rounded-md border-2 bg-slate-400 p-2 font-medium text-black">
+            <SignInButton>
+              Sign in with Github and leave a comment below
+            </SignInButton>
+          </div>
+        )}
         {user.isSignedIn && <SignOutButton />}
+
         <div>
           {data?.map((comment) => (
             <div key={comment.authorId}>{comment.content}</div>
