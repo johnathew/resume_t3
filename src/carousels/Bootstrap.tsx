0023b80cca69f606284ad608116c1a934427b0c1
johnathew <jakornegay01@gmail.com>
2023-05-24 14:10:20 -0500

    styled job history page

--- src/carousels/Bootstrap.tsx
+++ src/carousels/Bootstrap.tsx
@@ -107,8 +107,219 @@ export default function BootstrapCarousel() {
             </div>
           </div>
         </Carousel.Item>
-        <Carousel.Item className="h-full w-full" id="Job History">
-          <section className="relative flex h-full w-full flex-col overflow-y-scroll pt-12 text-white md:flex md:h-screen md:p-10 md:pt-14">
+        <Carousel.Item className="md:h-screen" id="Job History">
+          <section className="relative text-gray-600">
+
+            <h1 className="pt-20 -mb-8 bg-gradient-to-r from-[#71fead] to-[#437e60] bg-clip-text pl-4 font-bold text-transparent  md:pl-24">
+              Job History
+            </h1>
+            <div className="container mx-auto flex flex-wrap">
+              <div className="relative flex pb-2 pt-10 sm:items-center md:w-2/3">
+                <div className="absolute -inset-2 flex h-full mt-14 w-6 items-center justify-center">
+                  <div className="pointer-events-none h-full w-1 bg-gray-200"></div>
+                </div>
+                <div className="relative z-10 mt-10 inline-flex h-2 w-2 flex-shrink-0 items-center justify-center rounded-full border-2 bg-[#71fead] text-sm font-medium text-white sm:mt-0"></div>
+                <ul className="list-none text-sm text-[#71fead]">
+                  <li className="-mx-8 w-screen">
+                    <div className="flex h-1/2 w-full items-center md:mx-11">
+                      <h3 className="text-base font-bold mx-2">
+                        Helpdesk Specialist
+                      </h3>
+                      <span className="pt-2">
+                        <p className="text-slate-300">
+                          &nbsp; July 2021 - Present
+                        </p>
+                      </span>
+                    </div>
+                    <p className="-mt-3 flex mx-2 justify-between font-light italic md:pl-11">
+                      Maximus Federal - Remote; McAllen, TX
+                    </p>
+                    <ul className="-mx-4 w-full list-outside list-disc space-y-2 pb-2 text-slate-200 md:mx-7 md:w-3/4 md:space-y-0.5 md:text-base">
+                      <li>
+                        Troubleshoot official CDC software related issues for
+                        healthcare professionals and the public.
+                      </li>
+                      <li>
+                        Process escalation service request tickets as needed to
+                        ensure service levels are maintained.
+                      </li>
+                      <li>
+                        Coordinate with official CDC representatives to receive
+                        reports on system updates.
+                      </li>
+                      <li>
+                        Collaborate with other Helpdesk Specialists to share new
+                        procedures and/or critical information received from
+                        upper management.
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div className="relative flex pb-2 sm:items-center md:w-2/3">
+                <div className="absolute -inset-2 flex h-full w-6 items-center justify-center">
+                  <div className="pointer-events-none h-full w-1 bg-gray-200"></div>
+                </div>
+                <div className="title-font relative z-10 mt-10 inline-flex h-2 w-2 flex-shrink-0 items-center justify-center rounded-full bg-green-500 text-sm font-medium text-white sm:mt-0"></div>
+                <ul className="list-none text-sm text-[#71fead]">
+                  <li className="-mx-8 w-screen">
+                    <div className="flex h-1/2 w-full items-center md:mx-11">
+                      <h3 className="text-base font-bold mx-2">
+                        Teaching Assistant
+                      </h3>
+                      <span className="pt-2">
+                        <p className="text-slate-300">
+                          &nbsp; &nbsp;Spring 2021
+                        </p>
+                      </span>
+                    </div>
+                    <p className="-mt-3 mx-2 flex justify-between font-light italic md:pl-11">
+                      University of Texas Rio Grande Valley - Remote; Edinburg,
+                      TX
+                    </p>
+                    <ul className="-mx-4 w-full list-outside list-disc space-y-2 pb-2 text-slate-200 md:mx-7 md:w-3/4 md:space-y-0.5 md:text-base">
+                      <li>
+                        Assisted undergraduate Chemistry students with course
+                        material, holding weekly Q&A sessions
+                      </li>
+                      <li>
+                        Conducted online test reviews and held office hours for
+                        personal tutoring
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div className="relative flex pb-2 sm:items-center md:w-2/3">
+                <div className="absolute -inset-2 flex h-full w-6 items-center justify-center">
+                  <div className="pointer-events-none h-full w-1 bg-gray-200"></div>
+                </div>
+                <div className="title-font relative z-10 mt-10 inline-flex h-2 w-2 flex-shrink-0 items-center justify-center rounded-full bg-green-500 text-sm font-medium text-white sm:mt-0"></div>
+                <ul className="list-none text-sm text-[#71fead]">
+                  <li className="-mx-8 w-screen">
+                    <div className="flex h-1/2 w-full items-center md:mx-11">
+                      <h3 className="text-base font-bold mx-2">Survey Assistant</h3>
+                      &nbsp;
+                      <span className="pt-2">
+                        <p className="text-slate-300">
+                          &nbsp;October 2021 - December 2021
+                        </p>
+                      </span>
+                    </div>
+                    <p className="-mt-3 flex mx-2 justify-between font-light italic md:pl-11">
+                      SAMES, Inc. - McAllen, TX
+                    </p>
+                    <ul className="-mx-4 w-full list-outside list-disc space-y-2 pb-2 text-slate-200 md:mx-7 md:w-3/4 md:space-y-0.5 md:text-base">
+                      <li>
+                        Mapped out boundaries for businesses and residential
+                        homes using GPS technology
+                      </li>
+                      <li>
+                        Provided measurements and layouts for construction
+                        projects in both metropolitan and rural areas
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+              <div className="relative flex  sm:items-center md:w-2/3">
+                <div className="absolute -inset-2 flex h-full w-6 items-center justify-center">
+                  <div className="pointer-events-none h-full w-1 bg-gray-200"></div>
+                </div>
+                <div className="title-font relative z-10 mt-10 inline-flex h-2 w-2 flex-shrink-0 items-center justify-center rounded-full bg-green-500 text-sm font-medium text-white sm:mt-0"></div>
+                <ul className="list-none text-sm text-[#71fead]">
+                  <li className="-mx-8 w-screen">
+                    <div className="flex h-1/2 w-full items-center md:mx-11">
+                      <h3 className="text-base font-bold mx-2">Pharmacy Technician</h3>
+                      &nbsp;
+                      <span className="pt-2">
+                        <p className="text-slate-300">
+                          &nbsp;October 2016 - May 2020
+                        </p>
+                      </span>
+                    </div>
+                    <p className="-mt-3 flex justify-between font-light italic mx-2 md:pl-11">
+                    Walgreens Pharmacy- Palmview, TX
+                    </p>
+                    <ul className="-mx-4 w-full list-outside list-disc space-y-2 pb-6 text-slate-200 md:mx-7 md:w-3/4 md:space-y-0.5 md:text-base">
+                      <li>
+                      Utilized propriety software to manage patient’s prescriptions,
+                  resolve rejected insurance claims, and scheduled refills for
+                  medication
+                      </li>
+                      <li>
+                      Maintained strong attention to detail, making sure all
+                  patients received the correct prescription(s); professionally
+                  handled confidential patient information.
+                      </li>
+                      <li>
+                      Resolved any hardware related issues that inhibited normal
+                  pharmacy operations.
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </Carousel.Item>
+        <Carousel.Item className="h-screen w-full" id="Projects">
+          <section className="relative -m-1 flex h-full w-full flex-col overflow-scroll pt-12 text-white md:flex md:h-screen md:p-10 md:pt-14">
+            <h1 className="mr-2 bg-gradient-to-r from-[#71fead] to-[#437e60] bg-clip-text py-4 pl-4  font-bold text-transparent md:pl-1">
+              Projects
+            </h1>
+          </section>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+}
+
+{
+  /* <h1 className="text-lg font-bold underline">SAMES, Inc.</h1>
+<h2 className="flex justify-between italic">
+  Survey Assistant - McAllen, TX
+  <span>
+    <p>October 2021 - December 2021</p>
+  </span>
+</h2>
+<ul className="w-full list-inside list-disc pb-2 md:w-3/4">
+  <li>
+    Mapped out boundaries for businesses and residential homes using
+    GPS technology
+  </li>
+  <li>
+    Provided measurements and layouts for construction projects in
+    varying locations
+  </li>
+</ul>
+<h1 className="text-lg font-bold underline">Pharmacy Technician</h1>
+<h2 className="flex justify-between italic">
+  Walgreens Pharmacy- Palmview, TX
+  <span>
+    <p>October 2016 - May 2020</p>
+  </span>
+</h2>
+<ul className="w-full list-inside list-disc pb-2 md:w-3/4">
+  <li>
+    Utilized propriety software to manage patient’s prescriptions,
+    resolve rejected insurance claims, and scheduled refills for
+    medication
+  </li>
+  <li>
+    Maintained strong attention to detail, making sure all patients
+    received the correct prescription(s); professionally handled
+    confidential patient information.
+  </li>
+  <li>
+    Resolved any hardware related issues that inhibited normal
+    pharmacy operations.
+  </li>
+</ul> */
+}
+
+
+{/* <section className="container flex h-full w-full flex-col overflow-scroll pt-12 text-white md:flex md:h-screen md:p-10 md:pt-14">
             <h1 className="mr-2 bg-gradient-to-r from-[#71fead] to-[#437e60] bg-clip-text py-4 pl-4 font-bold text-transparent md:pl-16">
               Job History
             </h1>
@@ -194,8 +405,9 @@ export default function BootstrapCarousel() {
                 </li>
                 <li>
                   Provided measurements and layouts for construction projects in
-                  varying locations
+                  both metropolitan and rural areas
                 </li>
+              
               </ul>
               <ul className="list-item list-disc text-sm text-[#71fead]">
                 <li className="-mx-8 w-screen list-none">
@@ -230,59 +442,4 @@ export default function BootstrapCarousel() {
                 </li>
               </ul>
             </div>
-          </section>
-        </Carousel.Item>
-        <Carousel.Item className="h-screen w-full" id="Projects">
-          <section className="relative -m-1 flex h-full w-full flex-col overflow-scroll pt-12 text-white md:flex md:h-screen md:p-10 md:pt-14">
-            <h1 className="mr-2 bg-gradient-to-r from-[#71fead] to-[#437e60] bg-clip-text py-4 pl-4  font-bold text-transparent md:pl-1">
-              Projects
-            </h1>
-          </section>
-        </Carousel.Item>
-      </Carousel>
-    </>
-  );
-}
-
-{
-  /* <h1 className="text-lg font-bold underline">SAMES, Inc.</h1>
-<h2 className="flex justify-between italic">
-  Survey Assistant - McAllen, TX
-  <span>
-    <p>October 2021 - December 2021</p>
-  </span>
-</h2>
-<ul className="w-full list-inside list-disc pb-2 md:w-3/4">
-  <li>
-    Mapped out boundaries for businesses and residential homes using
-    GPS technology
-  </li>
-  <li>
-    Provided measurements and layouts for construction projects in
-    varying locations
-  </li>
-</ul>
-<h1 className="text-lg font-bold underline">Pharmacy Technician</h1>
-<h2 className="flex justify-between italic">
-  Walgreens Pharmacy- Palmview, TX
-  <span>
-    <p>October 2016 - May 2020</p>
-  </span>
-</h2>
-<ul className="w-full list-inside list-disc pb-2 md:w-3/4">
-  <li>
-    Utilized propriety software to manage patient’s prescriptions,
-    resolve rejected insurance claims, and scheduled refills for
-    medication
-  </li>
-  <li>
-    Maintained strong attention to detail, making sure all patients
-    received the correct prescription(s); professionally handled
-    confidential patient information.
-  </li>
-  <li>
-    Resolved any hardware related issues that inhibited normal
-    pharmacy operations.
-  </li>
-</ul> */
-}
+          </section> */}
