f0be13d1a31f135eb3df9e391cd126d2e94d15ef
johnathew <jakornegay01@gmail.com>
2023-06-21 13:41:09 -0500

    added GitHub links

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,36 +1,54 @@
 import React from "react";
 import { CgWebsite } from "react-icons/cg";
 import { BiCode } from "react-icons/bi";
+import Link from "next/link";
 
 type ProjectPropTypes = {
   projectName: string;
   description: string;
   languages: string;
+  ghLink: string;
+  previewLink: string;
 };
 
 const ProjectCard = ({
   projectName,
   description,
   languages,
+  previewLink,
+  ghLink,
 }: ProjectPropTypes) => {
   return (
     <>
-      <div className="relative flex flex-col w-full rounded-lg border-4 border-double border-black bg-gradient-to-r from-[#71fead] to-[#437e60] p-3 text-[10px] text-slate-950 shadow  duration-500 hover:m-2 hover:transform hover:p-2 md:h-full md:max-w-xl md:justify-between md:text-base md:hover:bg-[#71fead] md:hover:bg-none group">
-        <div className="flex flex-col align-middle items-center">
-          <h1 className="text-sm md:text-lg group-hover:underline underline-offset-4 font-extrabold">{projectName}</h1>
+      <div className="group relative flex w-full flex-col rounded-lg border-4 border-double border-black bg-gradient-to-r from-[#71fead] to-[#437e60] p-3 text-[10px] text-slate-950  shadow duration-500 hover:m-2 hover:transform hover:p-2 md:h-full md:max-w-xl md:justify-between md:text-base md:hover:bg-[#71fead] md:hover:bg-none">
+        <div className="flex flex-col items-center align-middle">
+          <h1 className="text-sm font-extrabold underline-offset-4 group-hover:underline md:text-lg">
+            {projectName}
+          </h1>
           <p className="font-normal">{description}</p>
           <p className="font-extralight">{languages}</p>
         </div>
         <div className="border-t-2 border-slate-800 border-opacity-40">
-          <ul className="flex pt-2 h-auto justify-center w-auto items-center gap-2 md:p-0.5">
-            <li className="flex flex-col items-center border-2  bg-slate-900 border-slate-200 text-slate-200 p-1 shadow rounded-md">
-              <CgWebsite className="transform rounded-lg text-lg duration-500 hover:bg-orange-400 md:text-2xl" />
-              <label className="font-normal text-sm">Live Preview</label>
-            </li>
-            <li className="flex flex-col items-center border-2 text-slate-200 border-slate-200 bg-black p-1 shadow rounded-md">
-              <BiCode className="transform rounded-lg text-lg duration-500 hover:bg-orange-400 md:text-2xl" />
-              <label  className="font-normal text-sm">GitHub Link</label>
-            </li>
+          <ul className="flex h-auto w-auto items-center justify-center gap-2 pt-2 md:p-0.5">
+            <Link
+              href={`${previewLink}`}
+              legacyBehavior
+            >
+              <a target="_blank" className="m-0 h-auto w-auto no-underline">
+                <li className="flex flex-col items-center rounded-md  border-2 border-slate-200 bg-slate-900 p-1 text-slate-200 shadow">
+                  <CgWebsite className="transform rounded-lg text-lg duration-500 hover:bg-orange-400 md:text-2xl" />
+                  <label className="text-sm font-normal">Live Preview</label>
+                </li>
+              </a>
+            </Link>
+            <Link href={`${ghLink}`} legacyBehavior>
+            <a target="_blank" className="m-0 h-auto w-auto no-underline">
+              <li className="flex flex-col items-center rounded-md border-2 border-slate-200 bg-black p-1 text-slate-200 shadow">
+                <BiCode className="transform rounded-lg text-lg duration-500 hover:bg-orange-400 md:text-2xl" />
+                <label className="text-sm font-normal">GitHub Link</label>
+              </li>
+              </a>
+            </Link>
           </ul>
         </div>
       </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,44 +8,36 @@ const Projects = () => {
       <h1 className="-mb-8 bg-gradient-to-r from-[#71fead] to-[#437e60] bg-clip-text pt-20 font-bold text-transparent">
         Projects
       </h1>
-      <div className="mt-14 flex flex-col gap-2 mb-10 md:mb-0 w-full justify-center rounded-lg px-4 md:mt-10 md:pt-5 md:grid md:h-full md:w-2/3 md:grid-cols-2 md:gap-2 md:flex-none">
-        <Link href="https://poetic-figolla-62a3eb.netlify.app/" legacyBehavior>
-          <a target="_blank" className="m-0 h-auto w-auto no-underline">
-            <ProjectCard
-              projectName="Pokedex"
-              languages="Typescript, React, Tailwind"
-              description="Pokedex made utilizing Pokemon-API"
-            />
-          </a>
-        </Link>
-        <Link href="https://poetic-figolla-62a3eb.netlify.app/" legacyBehavior>
-          <a target="_blank" className="m-0 h-auto w-auto p-0 no-underline">
-            <ProjectCard
-              projectName="GitHub Repo Search"
-              languages="Typescript, React, Tailwind"
-              description="Search GitHub repositories"
-            />
-          </a>
-        </Link>
+      <div className="mb-10 mt-14 flex w-full flex-col justify-center gap-2 rounded-lg px-4 md:mb-0 md:mt-10 md:grid md:h-full md:w-2/3 md:flex-none md:grid-cols-2 md:gap-2 md:pt-5">
+        <ProjectCard
+          projectName="Pokedex"
+          languages="Typescript, React, Tailwind"
+          description="Pokedex made utilizing Pokemon-API"
+          previewLink="https://poetic-figolla-62a3eb.netlify.app/"
+          ghLink="https://github.com/johnathew/pokemonAPI-reacthooks"
+        />
+        <ProjectCard
+          projectName="GitHub Repo Search"
+          languages="Typescript, React, Tailwind"
+          description="Search GitHub repositories"
+          previewLink="https://deluxe-cascaron-d7a13a.netlify.app/"
+          ghLink="https://github.com/johnathew/github_react_exercise_ts"
+        />
+        <ProjectCard
+          projectName="Memory Game"
+          languages="React, Javascript, Tailwind"
+          description="Memory Card game made with JS"
+          previewLink="https://poetic-figolla-62a3eb.netlify.app/"
+          ghLink="https://github.com/johnathew/memory_game"
+        />
 
-        <Link href="https://poetic-figolla-62a3eb.netlify.app/" legacyBehavior>
-          <a target="_blank" className="m-0 h-auto w-auto p-0 no-underline">
-            <ProjectCard
-              projectName="Memory Game"
-              languages="React, Javascript, Tailwind"
-              description="Memory Card game made with JS"
-            />
-          </a>
-        </Link>
-        <Link href="https://poetic-figolla-62a3eb.netlify.app/" legacyBehavior>
-          <a target="_blank" className="m-0 h-auto w-auto p-0 no-underline">
-            <ProjectCard
-              projectName="Resume"
-              languages="Typescript, Tailwind, Next JS"
-              description="Code used to create this site"
-            />
-          </a>
-        </Link>
+        <ProjectCard
+          projectName="Resume"
+          languages="Typescript, Tailwind, Next JS"
+          description="Code used to create this site"
+          previewLink="https://poetic-figolla-62a3eb.netlify.app/"
+          ghLink="https://github.com/johnathew/resume_t3"
+        />
       </div>
     </section>
   );
